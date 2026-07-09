257373442be4759ecdb47547cc029f15f32b444b
Kelvin McDonald <kelvin.mcdonald@joescan.com>
2026-07-09 11:19:39 -0700

    Fix builds for mingw

--- PinchotBuildApplication.cmake
+++ PinchotBuildApplication.cmake
@@ -6,10 +6,10 @@ include(PinchotSources)
 include(PinchotSchema)
 include(PinchotVersionInfo)
 
-if(WIN32)
+if(MSVC)
   set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /MT /EHsc")
   set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /MTd /EHsc")
-endif (WIN32)
+endif(MSVC)
 
 if(UNIX)
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wunused-variable -Wunused-result")
@@ -22,3 +22,8 @@ list (APPEND SOURCES ${C_API_SOURCES})
 list (APPEND SOURCES ${SCHEMA_BINARY_SOURCES})
 
 target_sources(${CMAKE_PROJECT_NAME} PRIVATE ${SOURCES})
+target_link_libraries(${CMAKE_PROJECT_NAME} ${CMAKE_THREAD_LIBS_INIT})
+
+if(WIN32)
+  target_link_libraries(${CMAKE_PROJECT_NAME} ws2_32 iphlpapi)
+endif(WIN32)
--- PinchotBuildLibrary.cmake
+++ PinchotBuildLibrary.cmake
@@ -6,10 +6,10 @@ include(PinchotSources)
 include(PinchotSchema)
 include(PinchotVersionInfo)
 
-if(WIN32)
+if(MSVC)
   set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_CXX_FLAGS_RELEASE} /MT /EHsc")
   set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} /MTd /EHsc")
-endif(WIN32)
+endif(MSVC)
 
 if(UNIX)
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wunused-variable -Wunused-result")
@@ -20,11 +20,22 @@ if(UNIX)
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fvisibility=hidden")
 endif(UNIX)
 
+if(MINGW)
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wunused-variable -Wunused-result")
+  # Same visibility semantics as UNIX; __declspec(dllexport) is still required
+  # on exported symbols but this prevents accidental export of internal symbols.
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fvisibility=hidden")
+endif(MINGW)
+
 target_sources(
   ${CMAKE_PROJECT_NAME}
   PRIVATE ${C_API_SOURCES} ${SCHEMA_BINARY_SOURCES}
 )
 target_link_libraries(${CMAKE_PROJECT_NAME} ${CMAKE_THREAD_LIBS_INIT})
+
+if(WIN32)
+  target_link_libraries(${CMAKE_PROJECT_NAME} ws2_32 iphlpapi)
+endif(WIN32)
 
 add_custom_command(
   TARGET ${CMAKE_PROJECT_NAME} POST_BUILD
